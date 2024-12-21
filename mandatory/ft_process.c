8cda92271a099f9ea46553ad2b09a7d7c0c9382f
Ilan Nowak--Glandier <inowak--@2D7.42angouleme.fr>
2024-12-21 14:15:35 +0100

    raccourci

--- mandatory/ft_process.c
+++ mandatory/ft_process.c
@@ -6,7 +6,7 @@
 /*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 10:40:26 by inowak--          #+#    #+#             */
-/*   Updated: 2024/12/21 14:12:33 by inowak--         ###   ########.fr       */
+/*   Updated: 2024/12/21 14:15:18 by inowak--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static int	search_command_exec(char *cmd, char **env)
 	}
 	pn = find_path(env, cmd);
 	if (!pn)
-		return (-2);
+		return (-1);
 	if (!exec_command(pn, cmd_a, env))
 		return (0);
 	else
@@ -106,11 +106,6 @@ void	process_child(t_pipex *pipex, int idx, t_data data)
 	close_pipes(pipex, data.argc - 4);
 	index_error = search_command_exec(data.argv[idx + 2], data.env);
 	if (index_error == -1)
-	{
-		perror("Error");
-		exit(1);
-	}
-	else if (index_error == -2)
 	{
 		perror("Error");
 		exit(1);
